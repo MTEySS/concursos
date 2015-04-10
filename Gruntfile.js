7f90e801f8701950af214008ce71f38320cb1baa
opensas <opensas@gmail.com>
2015-04-10 02:25:47 -0300

    adjust font size for smaller devices

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-
-
     concurrent: {
       devel: {
         tasks: ['connect:demo', 'watch'],
